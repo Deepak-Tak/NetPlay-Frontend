89d95a775aabb4dbe3874c5e0f372eb9ad9b2d0d
Deepak-Tak <deepaktak1444@gmail.com>
2024-07-21 22:54:28 +0530

    CORS ISSUE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ const DATA_API =
   "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=";
 export const KEY = process.env.REACT_APP_YOUTUBE_KEY;
 export const AUTOSUGGESION_API =
-  "https://cors-anywhere.herokuapp.com/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const SEARCHDATA_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&key=${KEY}&q=`;
 export const VIDEO_LIST_API = DATA_API + KEY;
 export const VIDEO_LIST_BY_IDS = `https://www.googleapis.com/youtube/v3/videos?key=${KEY}&part=snippet&id=`;
